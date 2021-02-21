99c9c887afdf9dc4cb9312cd82caa754f98f3f2a
alix <alix@MacB-bagam-baunez.local>
2021-02-21 02:16:23 +0100

    2021-02-21 Update

---
+++ R/atc_load.R
@@ -0,0 +1,9 @@
+#' Automatically load my most used packages
+#' @description Add more packages to the body to facilitate loading of
+#' multiple packages at the beginning of an R session
+#' @export
+
+atc_load <- function(){
+  library(rstatix)
+  library(tidyverse)
+}
